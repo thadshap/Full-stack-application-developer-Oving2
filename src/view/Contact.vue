0a647856595f74b79a896eac7a8fa053f0606e4d
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2022-02-24 17:03:30 +0100

    validation done

--- src/view/Contact.vue
+++ src/view/Contact.vue
@@ -4,14 +4,17 @@
       <h3>Contact form</h3>
       <label>Name:</label>
       <input id="name" v-model="contactInfo.name">
+      <p v-if="!nameIsValid" class="error-message">The name field is required</p>
 
       <label>E-post:</label>
       <input id="epost" v-model="contactInfo.epost">
+      <p v-if="!epostIsValid" class="error-message">The epost field is required</p>
 
       <label>Message:</label>
       <textarea id="message" v-model="contactInfo.message"></textarea>
+      <p v-if="!messageIsValid" class="error-message">The message field is required</p>
 
-      <button type="submit">Submit</button>
+      <button :disabled="!formIsValid" type="submit">Submit</button>
     </form>
   </div>
 </template>
@@ -31,9 +34,30 @@ export default {
       }
     }
   },
+  computed:{
+    nameIsValid(){
+      return !!this.contactInfo.name
+    },
+    epostIsValid(){
+      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return  !!this.contactInfo.epost && regex.test(String(this.contactInfo.epost).toLowerCase())
+
+    },
+    messageIsValid(){
+      return !!this.contactInfo.message
+    },
+    formIsValid(){
+      return this.nameIsValid && this.epostIsValid && this.messageIsValid
+    },
+  },
   inject:['GStore'],
   methods: {
     onSubmit() {
+      if(this.formIsValid){
+        console.log('Form Submitted', this.contactInfo)
+      }else{
+        console.log('Invalid form')
+      }
       const contactInfo={
         ...this.contactInfo,
         id : uuidv4(),
@@ -44,6 +68,9 @@ export default {
       setTimeout(() => {
         this.GStore.flashMessage = ''
       }, 3000)
+      document.getElementById("name").value = '';
+      document.getElementById("epost").value = ''
+      document.getElementById("message").value = '';
     }
   }
 }
